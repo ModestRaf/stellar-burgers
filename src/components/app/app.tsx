b93a21b3da6e8c4eea9d3411931b9852ce97385e
ModestRaf <tanya.stargate@yandex.ru>
2025-01-26 23:34:45 +0300

    обозначены пути в app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,70 @@
-import { ConstructorPage } from '@pages';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import ProtectedRoute from '../../services/ProtectedRoute';
 
-import { AppHeader } from '@components';
+const App = () => {
+  const handleCloseModal = () => {
+    console.log('Модальное окно закрыто');
+  };
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+  const isAuthenticated = false; // Временно для тестирования
+
+  return (
+    <Router>
+      <div className={styles.app}>
+        <AppHeader />
+        <Routes>
+          {/* Основные роуты */}
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+
+          {/* Защищённые маршруты */}
+          <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/profile/orders' element={<ProfileOrders />} />
+            <Route
+              path='/profile/orders/:number'
+              element={
+                <Modal title='Детали заказа' onClose={handleCloseModal}>
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+          </Route>
+
+          {/* Модалка ингредиента */}
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='Детали ингредиента' onClose={handleCloseModal}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+
+          {/* Роут для 404 */}
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
