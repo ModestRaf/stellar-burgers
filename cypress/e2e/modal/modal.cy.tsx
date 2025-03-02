b9db3c2037d60721438fa18100820b283e2d5b7e
ModestRaf <tanya.stargate@yandex.ru>
2025-03-02 12:16:33 +0300

    исправления по замечаниям

--- cypress/e2e/modal/modal.cy.tsx
+++ cypress/e2e/modal/modal.cy.tsx
@@ -1,13 +1,28 @@
-///<reference types="cypress"/>
+/// <reference types="cypress" />
 describe('Конструктор бургеров', () => {
   beforeEach(() => {
     cy.intercept('api/ingredients', { fixture: 'ingredients.json' });
     cy.visit('http://localhost:4000');
   });
-  it('Открытие модального окна', () => {
+  it('Открытие модального окна с проверкой данных ингредиента', () => {
     cy.get('[data-cy=modal]').should('not.exist');
-    cy.get('[data-cy=burger-constructor__ingredient]').eq(0).click();
-    cy.get('[data-cy=modal]');
+    // Получаем данные ингредиента из карточки (перед кликом)
+    cy.get('[data-cy=burger-constructor__ingredient]')
+      .eq(0)
+      .then(($ingredient) => {
+        const ingredientName = $ingredient
+          .find('[data-cy=ingredient-name]')
+          .text();
+        // Кликаем на ингредиент
+        cy.get('[data-cy=burger-constructor__ingredient]').eq(0).click();
+        cy.get('[data-cy=modal]').should('exist');
+        cy.get('[data-cy=modal]').within(() => {
+          cy.get('[data-cy=modal-ingredient-name]', { timeout: 5000 }).should(
+            'have.text',
+            ingredientName
+          );
+        });
+      });
   });
 
   it('Закрытие модального окна на крестик', () => {
